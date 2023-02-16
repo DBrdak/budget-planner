f27222954ef686b098befaccd7262de4d53b6d7c
DBrdak <brdak02dominik@gmail.com>
2023-02-17 00:11:07 +0100

    Seeder update

---
+++ Application/FutureTransactions/IncomesList.cs
@@ -0,0 +1,49 @@
+﻿using Application.Core;
+using Application.DTO;
+using Application.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.FutureTransactions
+{
+    public class IncomesList
+    {
+        public class Query : IRequest<Result<List<FutureIncomeDto>>>
+        {
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<FutureIncomeDto>>>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IUserAccessor userAccessor, IMapper mapper)
+            {
+                _context = context;
+                _userAccessor = userAccessor;
+                _mapper = mapper;
+            }
+
+            async Task<Result<List<FutureIncomeDto>>> IRequestHandler<Query, Result<List<FutureIncomeDto>>>.Handle(Query request, CancellationToken cancellationToken)
+            {
+                var futureTransactions = await _context.FutureTransactions
+                    .AsNoTracking()
+                    .Where(ft => ft.Budget.User.UserName == _userAccessor.GetUsername())
+                    .ProjectTo<FutureIncomeDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return Result<List<FutureIncomeDto>>.Success(futureTransactions);
+            }
+        }
+    }
+}
--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -13,7 +13,8 @@ namespace Persistence
         public static async Task SeedData(DataContext context,
             UserManager<User> userManager)
         {
-            if (!userManager.Users.Any() && !context.Budgets.Any())
+            if (!userManager.Users.Any() && !context.Budgets.Any() && !context.Goals.Any() && !context.Accounts.Any() &&
+                !context.FutureTransactions.Any() && !context.FutureSavings.Any() && !context.Transactions.Any() && !context.Savings.Any())
             {
                 var johnAccounts = new List<Account>
                 {
@@ -101,32 +102,141 @@ namespace Persistence
                 budgets[1].User = users[1];
                 budgets[2].User = users[2];
 
-                await context.Accounts.AddRangeAsync(johnAccounts);
-                await context.Budgets.AddRangeAsync(budgets);
-                await context.SaveChangesAsync();
-            }
-            if (!context.Goals.Any())
-            {
-                var date = DateTime.UtcNow.AddDays(300);
+                var ffdate = DateTime.UtcNow.AddDays(300);
                 var goals = new List<Goal> {
                     new Goal
                     {
                         Name = "chuj",
-                        EndDate = date,
+                        EndDate = ffdate,
                         CurrentAmount = 234.21,
                         RequiredAmount = 222222.12,
-                        BudgetId = Guid.Parse("58661662-D123-4249-8E82-184F8A4D1784")
+                        BudgetId = budgets[0].Id
                     },
                     new Goal
                     {
                         Name = "chujec",
-                        EndDate = date,
+                        EndDate = ffdate.AddDays(325),
                         CurrentAmount = 1234.21,
                         RequiredAmount = 2112222.12,
-                        BudgetId = Guid.Parse("58661662-D123-4249-8E82-184F8A4D1784")
+                        BudgetId = budgets[0].Id
                     }
                 };
+
+                var fdate = DateTime.UtcNow.AddDays(20);
+                var date = new DateTime(2022, 12, 1);
+
+                var fSav = new List<FutureSaving>
+                {
+                    new FutureSaving
+                    {
+                        FromAccount=johnAccounts[0],
+                        ToAccount=johnAccounts[2],
+                        Amount=1201.21,
+                        Goal = goals[0],
+                        Date = fdate,
+                        Budget=budgets[0]
+                    },
+                    new FutureSaving
+                    {
+                        FromAccount=johnAccounts[1],
+                        ToAccount=johnAccounts[2],
+                        Amount=172.32,
+                        Frequency="Monthly",
+                        Goal = goals[1],
+                        Date = fdate.AddDays(5),
+                        Budget=budgets[0]
+                    },
+                };
+
+                var fTran = new List<FutureTransaction>
+                {
+                    new FutureTransaction
+                    {
+                        Category="Groceries",
+                        Frequency="Weekly",
+                        Amount=-100,
+                        Account=johnAccounts[0],
+                        Budget=budgets[0],
+                        Date=fdate
+                    },
+                    new FutureTransaction
+                    {
+                        Category="Drugs",
+                        Amount=-8,
+                        Account=johnAccounts[1],
+                        Budget=budgets[0],
+                        Date=fdate.AddDays(14)
+                    },
+                    new FutureTransaction
+                    {
+                        Category="Job",
+                        Frequency="Monthly",
+                        Amount=1500,
+                        Account=johnAccounts[0],
+                        Budget=budgets[0],
+                        Date=fdate.AddDays(20)
+                    },
+                    new FutureTransaction
+                    {
+                        Category="Blowjob",
+                        Amount=200,
+                        Account=johnAccounts[1],
+                        Budget=budgets[0],
+                        Date=fdate.AddDays(1)
+                    }
+                };
+
+                var sav = new List<Saving>
+                {
+                    new Saving
+                    {
+                        Budget= budgets[0],
+                        Date= date.AddDays(1),
+                        Amount=50,
+                        FromAccount=johnAccounts[0],
+                        ToAccount=johnAccounts[2],
+                        Goal=goals[1]
+                    },
+                    new Saving
+                    {
+                        Budget= budgets[0],
+                        Date= date.AddDays(11),
+                        Amount=200,
+                        FromAccount=johnAccounts[1],
+                        ToAccount=johnAccounts[2],
+                        Goal=goals[0]
+                    }
+                };
+
+                var tran = new List<Transaction>
+                {
+                    new Transaction
+                    {
+                        Budget= budgets[0],
+                        Date= date.AddDays(13),
+                        Amount=50,
+                        Title="Biedra",
+                        Category="Groceries",
+                        Account=johnAccounts[0],
+                    },
+                    new Transaction
+                    {
+                        Budget= budgets[0],
+                        Date= date.AddDays(28),
+                        Amount=50,
+                        Title="Mefedron",
+                        Category="Drugs",
+                        Account=johnAccounts[1],
+                    }
+                };
+
+                await context.Transactions.AddRangeAsync(tran);
+                await context.Savings.AddRangeAsync(sav);
+                await context.FutureTransactions.AddRangeAsync(fTran);
+                await context.FutureSavings.AddRangeAsync(fSav);
                 await context.Goals.AddRangeAsync(goals);
+                await context.Accounts.AddRangeAsync(johnAccounts);
+                await context.Budgets.AddRangeAsync(budgets);
                 await context.SaveChangesAsync();
             }
         }
