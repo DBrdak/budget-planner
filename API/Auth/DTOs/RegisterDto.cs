e8488b0fe0594d25ef32636ee561435b129f9825
DBrdak <brdak02dominik@gmail.com>
2023-02-20 21:01:57 +0100

    User Remove, Edit And Register Function Added

--- API/Auth/DTOs/RegisterDto.cs
+++ API/Auth/DTOs/RegisterDto.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,23 @@ namespace API.Auth.DTOs
 {
     public class RegisterDto
     {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$", ErrorMessage = "Password must be complex")]
+        public string Password { get; set; }
+
+        [Required]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string DisplayName { get; set; }
+
+        [Required]
+        public string BudgetName { get; set; }
     }
 }
--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -2,11 +2,13 @@
 using API.Auth.DTOs;
 using Application.Interfaces;
 using Domain;
+using Infrastructure.Security;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Persistence;
 using System.Security.Claims;
 
 namespace API.Controllers
@@ -15,11 +17,13 @@ namespace API.Controllers
     [Route("api")]
     public class UserController : ControllerBase
     {
+        private readonly DataContext _context;
         private readonly UserManager<User> _userManager;
         private readonly TokenService _tokenService;
 
-        public UserController(UserManager<User> userManager, TokenService tokenService)
+        public UserController(DataContext context, UserManager<User> userManager, TokenService tokenService)
         {
+            _context = context;
             _userManager = userManager;
             _tokenService = tokenService;
         }
@@ -42,10 +46,66 @@ namespace API.Controllers
             return BadRequest();
         }
 
-        // Register controller =>
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto dto)
+        {
+            var usernameIsUnique = await _context.Users.AnyAsync(u => u.UserName == dto.Username);
+            var emailIsUnique = await _context.Users.AnyAsync(u => u.Email == dto.Email);
+            var budgetIsUnique = await _context.Budgets.AnyAsync(b => b.Name == dto.Username);
+
+            if (usernameIsUnique)
+            {
+                ModelState.AddModelError("username", "Username taken");
+                return ValidationProblem();
+            }
+
+            if (emailIsUnique)
+            {
+                ModelState.AddModelError("email", "Email taken");
+                return ValidationProblem();
+            }
+
+            if (budgetIsUnique)
+            {
+                ModelState.AddModelError("budgetName", "Budget name taken");
+                return ValidationProblem();
+            }
+
+            if (dto.Password != dto.ConfirmPassword)
+            {
+                ModelState.AddModelError("confirmPassword", "Passwords must be equal");
+                return ValidationProblem();
+            }
+
+            var user = new User
+            {
+                UserName = dto.Username,
+                Email = dto.Email,
+                DisplayName = dto.DisplayName
+            };
+
+            var result = await _userManager.CreateAsync(user, dto.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            var budget = new Budget
+            {
+                Name = dto.BudgetName,
+                User = user
+            };
+
+            await _context.AddAsync(budget);
+            var fail = await _context.SaveChangesAsync() < 0;
+
+            if (fail)
+                return BadRequest("Problem while creating budget");
+
+            return CreateUserObject(user);
+        }
 
         [Authorize] // Autoryzacja jedynie właściciela konta
-        [HttpGet("profile")]
+        [HttpGet("user")]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
             var user = await _userManager.Users
