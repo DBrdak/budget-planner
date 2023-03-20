f3e3b56bd5ab4b720fecbe53a1b9572197469c75
DBrdak <brdak02dominik@gmail.com>
2023-03-20 22:06:47 +0100

    Persistence XML Comment Added

--- Src/Persistence/DataContext.cs
+++ Src/Persistence/DataContext.cs
@@ -27,8 +27,43 @@ public class DataContext : IdentityDbContext<User>
 
         base.OnModelCreating(builder);
     }
-    
 
+
+    /// <summary>
+    ///     Saves all changes made in this context to the database.
+    ///     Additionally changes existing data when specified tables gets new data.
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         This method will automatically call <see cref="M:Microsoft.EntityFrameworkCore.ChangeTracking.ChangeTracker.DetectChanges" />
+    ///         to discover any changes to entity instances before saving to the underlying database. This can be disabled via
+    ///         <see cref="P:Microsoft.EntityFrameworkCore.ChangeTracking.ChangeTracker.AutoDetectChangesEnabled" />.
+    ///     </para>
+    ///     <para>
+    ///         Entity Framework Core does not support multiple parallel operations being run on the same DbContext instance. This
+    ///         includes both parallel execution of async queries and any explicit concurrent use from multiple threads.
+    ///         Therefore, always await async calls immediately, or use separate DbContext instances for operations that execute
+    ///         in parallel. See <see href="https://aka.ms/efcore-docs-threading">Avoiding DbContext threading issues</see> for more
+    ///         information and examples.
+    ///     </para>
+    ///     <para>
+    ///         See <see href="https://aka.ms/efcore-docs-saving-data">Saving data in EF Core</see> for more information and examples.
+    ///     </para>
+    /// </remarks>
+    /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken" /> to observe while waiting for the task to complete.</param>
+    /// <returns>
+    ///     A task that represents the asynchronous save operation. The task result contains the
+    ///     number of state entries written to the database.
+    /// </returns>
+    /// <exception cref="T:Microsoft.EntityFrameworkCore.DbUpdateException">
+    ///     An error is encountered while saving to the database.
+    /// </exception>
+    /// <exception cref="T:Microsoft.EntityFrameworkCore.DbUpdateConcurrencyException">
+    ///     A concurrency violation is encountered while saving to the database.
+    ///     A concurrency violation occurs when an unexpected number of rows are affected during save.
+    ///     This is usually because the data in the database has been modified since it was loaded into memory.
+    /// </exception>
+    /// <exception cref="T:System.OperationCanceledException">If the <see cref="T:System.Threading.CancellationToken" /> is canceled.</exception>
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
     {
         var numberOfChanges = ChangeTracker.Entries<FutureTransaction>().Count();
@@ -37,24 +72,25 @@ public class DataContext : IdentityDbContext<User>
         {
             var entry = ChangeTracker.Entries<FutureTransaction>().ElementAtOrDefault(i);
 
-            switch (entry.State)
-            {
-                case EntityState.Deleted:
-                    var category = TransactionCategories.FirstOrDefault
-                        (x => x.BudgetId == entry.Entity.BudgetId && x.Value == entry.Entity.Category);
-                    TransactionCategories.Remove(category);
-                    break;
+            if (entry != null)
+                switch (entry.State)
+                {
+                    case EntityState.Deleted:
+                        var category = TransactionCategories.FirstOrDefault
+                            (x => x.BudgetId == entry.Entity.BudgetId && x.Value == entry.Entity.Category);
+                        if (category != null) TransactionCategories.Remove(category);
+                        break;
 
-                case EntityState.Added:
-                    category = new TransactionCategory
-                    {
-                        BudgetId = entry.Entity.BudgetId,
-                        Value = entry.Entity.Category,
-                        Type = entry.Entity.Amount > 0 ? "income" : "expenditure"
-                    };
-                    ChangeTracker.Context.Attach(category);
-                    break;
-            }
+                    case EntityState.Added:
+                        category = new TransactionCategory
+                        {
+                            BudgetId = entry.Entity.BudgetId,
+                            Value = entry.Entity.Category,
+                            Type = entry.Entity.Amount > 0 ? "income" : "expenditure"
+                        };
+                        ChangeTracker.Context.Attach(category);
+                        break;
+                }
         }
 
         foreach (var entry in ChangeTracker.Entries<Transaction>())
@@ -80,7 +116,7 @@ public class DataContext : IdentityDbContext<User>
 
                     if (futureTransaction is not null)
                         futureTransaction.CompletedAmount -= amount;
-                    account.Balance -= amount;
+                    if (account != null) account.Balance -= amount;
                     break;
             }
 
@@ -118,8 +154,8 @@ public class DataContext : IdentityDbContext<User>
                         futureSaving.CompletedAmount -= amount;
                     if (goal is not null)
                         goal.CurrentAmount -= amount;
-                    sourceAccount.Balance += amount;
-                    destinationAccount.Balance -= amount;
+                    if (sourceAccount != null) sourceAccount.Balance += amount;
+                    if (destinationAccount != null) destinationAccount.Balance -= amount;
                     break;
             }
 
