7db1a433769bcf0efd9ff5b904b8c0f3b5ee1836
DBrdak <brdak02dominik@gmail.com>
2023-05-06 21:58:38 +0200

    Main Page Update

--- Src/WebUI/src/features/main/dashboard/MainPage.tsx
+++ Src/WebUI/src/features/main/dashboard/MainPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Grid, Card, Icon, Segment, Container, Button, CardHeader, Header, Image } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
@@ -7,10 +6,11 @@ const MainPage = () => {
     <Container fluid style={{marginTop:'12px'}}>
       <Grid centered verticalAlign='middle' style={{ height: '100vh'}}>
         <Grid.Row columns={3} verticalAlign='bottom'>
-          <Grid.Column width={4}>
+          <Grid.Column width={4} >
           <Button 
           fluid color='green' size='massive' 
-          style={{alignItems: 'center', borderRadius: '50px' }}>
+          style={{alignItems: 'center', borderRadius: '50px', 
+                  boxShadow:'0px 0px 7px 4px rgba(0, 0, 0, 0.25)', padding:'30px'}}>
             <Header inverted>Accounts</Header>
             <div style={{ marginTop: 'auto' }}>
               <Icon name='university' size='big'/>
@@ -20,7 +20,8 @@ const MainPage = () => {
           <Grid.Column width={4} textAlign='center'>
             <Button 
             fluid color='green' size='massive' 
-            style={{alignItems: 'center', borderRadius: '50px' }}>
+            style={{alignItems: 'center', borderRadius: '50px', 
+                    boxShadow:'0px 0px 7px 4px rgba(0, 0, 0, 0.25)', padding:'30px'}}>
               <Header inverted>Goals</Header>
               <div style={{ marginTop: 'auto' }}>
                 <Icon name='bullseye' size='big'/>
@@ -30,7 +31,8 @@ const MainPage = () => {
           <Grid.Column width={4} textAlign='center'>
             <Button 
             fluid color='green' size='massive' 
-            style={{alignItems: 'center', borderRadius: '50px' }}>
+            style={{alignItems: 'center', borderRadius: '50px', 
+                    boxShadow:'0px 0px 7px 4px rgba(0, 0, 0, 0.25)', padding:'30px'}}>
               <Header inverted>Profile</Header>
               <div style={{ marginTop: 'auto' }}>
                 <Icon name='user' size='big'/>
@@ -42,18 +44,20 @@ const MainPage = () => {
           <Grid.Column width={4} textAlign='center'>
             <Button 
             fluid size='massive' color='green'
-            style={{alignItems: 'center', borderRadius: '50px' }}>
+            style={{alignItems: 'center', borderRadius: '50px', 
+                    boxShadow:'0px 0px 7px 4px rgba(0, 0, 0, 0.25)'}}>
               <Header inverted>Spending Plan</Header>
               <div style={{ marginTop: 'auto' }}>
                 <Icon name='calculator' size='big'/>
               </div>
             </Button>
           </Grid.Column>
-          <Grid.Column width={4} textAlign='center' >
+          <Grid.Column width={4} textAlign='center' style={{gap:'50px'}} >
             <Button.Group fluid vertical>
               <Button 
               color='green' size='mini' 
-              style={{alignItems: 'center', borderRadius:'30px', justifyContent:'center', textAlign:'center'}}>
+              style={{alignItems: 'center', borderRadius:'30px', justifyContent:'center',
+                      textAlign:'center', boxShadow: "0px 0px 7px 2px rgba(0, 0, 0, 0.25)"}}>
                 Income
                 <div style={{ marginTop: 'auto', marginLeft:'5px'}}>
                   <Icon name='plus' size='big'/>
@@ -61,7 +65,8 @@ const MainPage = () => {
               </Button>
               <Button 
               color='green' size='mini' 
-              style={{alignItems: 'center', margin:'3px 0px 3px 0px', borderRadius:'30px', justifyContent:'center', textAlign:'center' }}>
+              style={{alignItems: 'center', margin:'7px 0px 7px 0px', borderRadius:'30px', justifyContent:'center', 
+                      textAlign:'center', boxShadow: "0px 0px 7px 2px rgba(0, 0, 0, 0.25)" }}>
                 Expenditure
                 <div style={{ marginTop: 'auto', marginLeft:'5px' }}>
                   <Icon name='minus' size='big'/>
@@ -69,7 +74,8 @@ const MainPage = () => {
               </Button>
               <Button 
               color='green' size='mini' 
-              style={{alignItems: 'center', borderRadius:'30px', justifyContent:'center', textAlign:'center'}}>
+              style={{alignItems: 'center', borderRadius:'30px', justifyContent:'center', 
+                      textAlign:'center', boxShadow: "0px 0px 7px 2px rgba(0, 0, 0, 0.25)"}}>
                 Saving
                 <div style={{ marginTop: 'auto', marginLeft:'5px' }}>
                   <Icon name='shield' size='big'/>
