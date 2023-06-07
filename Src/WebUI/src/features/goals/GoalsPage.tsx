369962b8112b9a58af7c3f00c24840ae7f251079
AStaniszewski <staniszewski01adam@gmail.com>
2023-06-07 15:25:03 +0200

    Goals update

--- Src/WebUI/src/features/goals/GoalsPage.tsx
+++ Src/WebUI/src/features/goals/GoalsPage.tsx
@@ -1,29 +1,24 @@
+import axios from 'axios';
 import { observer } from 'mobx-react-lite';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Card, Container, Grid } from 'semantic-ui-react';
 
 const GoalsPage = () => {
-  const [goals, setGoals] = useState([
-    {
-      id: 1,
-      title: "Goal 1",
-      description: "Description 1",
-      currentAmount: "Current Amount 1",
-      requiredAmount: "Required Amount 1",
-      endDate: "End Date 1",
-      showDetails: false,
-    },
-    {
-      id: 2,
-      title: "Goal 2",
-      description: "Description 2",
-      currentAmount: "Current Amount 2",
-      requiredAmount: "Required Amount 2",
-      endDate: "End Date 2",
-      showDetails: false,
-    },
-  ]);
+  const [goals, setGoals] = useState([]);
+
+  useEffect(() => {
+    const fetchGoals = async() => {
+      try {
+        const response = await axios.get('/goals')
+        setGoals(response.data.goals);
+      } catch (error) {
+        console.error('Error fetching goals', error);
+      }
+    };
+    fetchGoals();
+  }, []);
+    
 
   const toggleGoalDetails = (goalId: number) => {
     const updatedGoals = goals.map((goal) =>
@@ -34,46 +29,51 @@ const GoalsPage = () => {
 
   return (
     <Container>
-      <div style={{ fontSize: "50px", textAlign: "center", marginTop: "50px" }}>
+      <div style={{ fontSize: '50px', textAlign: 'center', marginTop: '50px' }}>
         Goals
       </div>
-      <Grid centered style={{ marginTop: "50px" }}>
-        <Grid.Row columns={1} doubling stackable>
-          {goals.map((goal) => (
-            <Grid.Column key={goal.id} style={{ textAlign: "center" }}>
-              <div style={{ marginBottom: "20px" }}>
-                <Card
-                  style={{
-                    width: "400px",
-                    margin: "0 auto",
-                    border: goal.showDetails ? "2px solid green" : "none",
-                  }}
-                  onMouseEnter={() => toggleGoalDetails(goal.id)}
-                  onMouseLeave={() => toggleGoalDetails(goal.id)}
-                >
-                  <Card.Content>
-                    <Card.Header
-                      style={{ fontSize: "20px", cursor: "pointer" }}
-                    >
-                      {goal.title}
-                    </Card.Header>
-                    <Card.Description
-                      style={{ marginTop: "20px", display: goal.showDetails ? "block" : "none" }}
-                    >
-                      <ul style={{ listStyle: "none", padding: 0 }}>
-                        <li>{goal.currentAmount}</li>
-                        <li>{goal.requiredAmount}</li>
-                        <li>{goal.endDate}</li>
-                        <li>{goal.description}</li>
-                      </ul>
-                    </Card.Description>
-                  </Card.Content>
-                </Card>
-              </div>
-            </Grid.Column>
-          ))}
-        </Grid.Row>
-      </Grid>
+      {goals.length === 0 ? (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>No goals found</div>
+      ) : (
+        <Grid centered style={{ marginTop: '50px' }}>
+          <Grid.Row columns={1} doubling stackable>
+            {goals.map((goal) => (
+              <Grid.Column key={goal.id} style={{ textAlign: 'center' }}>
+                <div style={{ marginBottom: '20px' }}>
+                  <Card
+                    style={{
+                      width: '400px',
+                      margin: '0 auto',
+                      border: goal.showDetails ? '2px solid green' : 'none',
+                    }}
+                    onMouseEnter={() => toggleGoalDetails(goal.id)}
+                    onMouseLeave={() => toggleGoalDetails(goal.id)}
+                  >
+                    <Card.Content>
+                      <Card.Header style={{ fontSize: '20px', cursor: 'pointer' }}>
+                        {goal.title}
+                      </Card.Header>
+                      <Card.Description
+                        style={{
+                          marginTop: '20px',
+                          display: goal.showDetails ? 'block' : 'none',
+                        }}
+                      >
+                        <ul style={{ listStyle: 'none', padding: 0 }}>
+                          <li>{goal.currentAmount}</li>
+                          <li>{goal.requiredAmount}</li>
+                          <li>{goal.endDate}</li>
+                          <li>{goal.description}</li>
+                        </ul>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                </div>
+              </Grid.Column>
+            ))}
+          </Grid.Row>
+        </Grid>
+      )}
     </Container>
   );
 };
